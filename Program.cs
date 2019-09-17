00f364d57941632425ec7c49c7f77fd27ef68e53
Erick Sanchez <sanchezerick27@icloud.com>
2019-09-17 09:08:19 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ericksanchez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
